d170ad872ec5ac7f9b6f019aaabd4f26640ae26c
Larissaantiquera <00001096707354SP@al.educacao.sp.gov.br>
2024-11-25 14:04:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 9L em mL?',
+    '9L em mL é 9000 mL'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 2 elevado a 8?',
+    '2 elevado a 8 é 256'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz preto em Inglês?',
+    'preto em inglês é black'
+)
